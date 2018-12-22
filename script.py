abf0f542e9a4fe900cd6d3a2ad9b4e38935a49f4
griimnak <griimnak@gmail.com>
2018-12-22 16:33:00 -0500

    improvements
    OPEN_WHEN_COMPLETE option added, BORDER option added, show_all_options()
    function added

--- script.py
+++ script.py
@@ -1,67 +1,108 @@
-import random
-
 """ pip install Pillow """
+import os
+import random
 from PIL import Image, ImageDraw, ImageFont
+CONFIG = {}
 
 # Fill these values
-USER = "Griimnak"
-AWARD = "Friendliest User"
+CONFIG["USER"] = "Griimnak"
+CONFIG["AWARD"] = "Friendliest User"
+
+CONFIG["OPEN_WHEN_COMPLETE"] = True
+# Should I open the image when done? True or False
 
 # These default values should be okay.
-AWARD_YEAR_TEXT = "2018 Community Awards"
-DEVBEST_TEXT = "DevBest.com"
+CONFIG["AWARD_YEAR_TEXT"] = "2018 Community Awards"
+CONFIG["DEVBEST_TEXT"] = "DevBest.com"
 
-USER_X_POS = 50
-USER_Y_POS = 15
+CONFIG["USER_X_POS"] = 50
+CONFIG["USER_Y_POS"] = 15
 
-AWARD_X_POS = 50
-AWARD_Y_POS = 31
+CONFIG["AWARD_X_POS"] = 50
+CONFIG["AWARD_Y_POS"] = 31
 
-AWARD_YEAR_X_POS = 350
-AWARD_YEAR_Y_POS = 15
+CONFIG["AWARD_YEAR_X_POS"] = 350
+CONFIG["AWARD_YEAR_Y_POS"] = 15
 
-DEVBEST_TEXT_X_POS = 418
-DEVBEST_TEXT_Y_POS = 31
+CONFIG["DEVBEST_TEXT_X_POS"] = 418
+CONFIG["DEVBEST_TEXT_Y_POS"] = 31
+
+CONFIG["BORDER"] = True
+CONFIG["BORDER_OUTER_COLOR"] = "black"
+CONFIG["BORDER_INNER_COLOR"] = "white"
 
 # Assets
-MEDAL_IMAGE = "assets/medal2.png"
-BG_IMAGE = "assets/bg3.png"
+CONFIG["MEDAL_IMAGE"] = "assets/medal2.png"
+CONFIG["BG_IMAGE"] = "assets/bg.png"
 
-STANDARD_TTF = "assets/Roboto-Regular.ttf"
-BOLD_TTF = "assets/Roboto-Bold.ttf"
+CONFIG["STANDARD_TTF"] = "assets/Roboto-Regular.ttf"
+CONFIG["BOLD_TTF"] = "assets/Roboto-Bold.ttf"
 
+
+""" Do not modify anything below unless you know what you're doing. """
+
+def show_all_options():
+    """ Prints all options to console """
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    for key in CONFIG:
+        print(key + "  = ", CONFIG[key])
 
 def main():
-    """ Script start """
-    image = Image.open(BG_IMAGE).convert("RGBA")
-    std = ImageFont.truetype(STANDARD_TTF, 12)
-    bld = ImageFont.truetype(BOLD_TTF, 12)
+    """ Creates the image """
+    image = Image.open(CONFIG["BG_IMAGE"]).convert("RGBA")
+    std = ImageFont.truetype(CONFIG["STANDARD_TTF"], 12)
+    bld = ImageFont.truetype(CONFIG["BOLD_TTF"], 12)
     try:
         layer1 = ImageDraw.Draw(image)
-        medal = Image.open(MEDAL_IMAGE).convert("RGBA")
+        if CONFIG["BORDER"] is True:
+            layer1.rectangle(
+                ((1, 1), (498, 58)),
+                width=1,
+                outline=CONFIG["BORDER_INNER_COLOR"])
+            layer1.rectangle(
+                ((0, 0), (499, 59)),
+                width=1,
+                outline=CONFIG["BORDER_OUTER_COLOR"])
+
+        medal = Image.open(CONFIG["MEDAL_IMAGE"]).convert("RGBA")
         image.paste(medal, (10, 15), medal)
 
         layer1.text(
-            (AWARD_YEAR_X_POS, AWARD_YEAR_Y_POS),
-            AWARD_YEAR_TEXT,
+            (CONFIG["AWARD_YEAR_X_POS"], CONFIG["AWARD_YEAR_Y_POS"]),
+            CONFIG["AWARD_YEAR_TEXT"],
             fill=(255, 255, 255),
             font=bld)
 
         layer1.text(
-            (DEVBEST_TEXT_X_POS, DEVBEST_TEXT_Y_POS),
-            DEVBEST_TEXT,
+            (CONFIG["DEVBEST_TEXT_X_POS"], CONFIG["DEVBEST_TEXT_Y_POS"]),
+            CONFIG["DEVBEST_TEXT"],
             fill=(255, 255, 255),
             font=std)
 
-        layer1.text((USER_X_POS, USER_Y_POS), USER, fill=(255, 255, 255), font=bld)
-        layer1.text((AWARD_X_POS, AWARD_Y_POS), AWARD, fill=(255, 255, 255), font=std)
+        layer1.text(
+            (CONFIG["USER_X_POS"], CONFIG["USER_Y_POS"]),
+            CONFIG["USER"],
+            fill=(255, 255, 255),
+            font=bld)
+
+        layer1.text(
+            (CONFIG["AWARD_X_POS"], CONFIG["AWARD_Y_POS"]),
+            CONFIG["AWARD"],
+            fill=(255, 255, 255),
+            font=std)
+
     except Exception as error:
         print(str(error))
         exit()
     finally:
-        out = 'samples/banner_sample%s_.png' % random.randint(0,1000)
+        out = 'samples/banner_sample%s_.png' % random.randint(0, 1000)
         image.save(out)
-    print("~ ./%s generated." % out)
+        if CONFIG["OPEN_WHEN_COMPLETE"] is True:
+            image.show(open(out, 'rb'))
+
+    show_all_options()
+    print("\n~ ./%s generated." % out)
 
 if __name__ == '__main__':
     main()
